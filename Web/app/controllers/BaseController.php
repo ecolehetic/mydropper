111bdf57fa28c88aeb6a46bd3194ec1c000eb7d7
Mathieu <mathieu.letyrant@gmail.com>
2015-01-30 16:32:50 +0100

    Back - Little Cosmetic :)

--- Web/app/controllers/BaseController.php
+++ Web/app/controllers/BaseController.php
@@ -12,11 +12,10 @@ class BaseController
     private $twig;
     protected $f3;
     protected $web;
-    private $view;
-    public $layout = 'layout';
     protected $action;
     protected $controller;
     protected $method;
+    public $layout = 'layout';
 
     /**
      * Return in all Child Constructor $twig, $f3, $web
@@ -35,12 +34,13 @@ class BaseController
      */
     protected function render($file, $values = [])
     {
-        if($file == true){
-            $tpl = $this->controller.'/'.$this->action.'.twig';
-        }elseif($file == false){
+        if ($file == true) {
+            $tpl = $this->controller . '/' . $this->action . '.twig';
+        } elseif ($file == false) {
             echo json_encode($values);
+
             return;
-        }else{
+        } else {
             $tpl = $file;
         }
         $values['layout'] = $this->layout;
@@ -52,32 +52,33 @@ class BaseController
      * Get the targeted template by url
      * Set as protected $action, $controller & $method
      */
-    private function getTpl(){
+    private function getTpl()
+    {
 
         $this->method = $this->f3['SERVER']['REQUEST_METHOD'];
 
         $params = $this->f3['PARAMS'];
         $base = $this->f3['PARAMS'][0];
-        if(count($this->f3['PARAMS'])>1){
+        if (count($this->f3['PARAMS']) > 1) {
             unset($params[0]);
-            foreach ($params as $k => $v){
-                $base = str_replace('/'.$v, '', $base);
-                $index = $base.'/@'.$k;
+            foreach ($params as $k => $v) {
+                $base = str_replace('/' . $v, '', $base);
+                $index = $base . '/@' . $k;
             }
-        }else{
+        } else {
             $index = $base;
         }
 
         foreach ($this->f3['ROUTES'] as $key => $value) {
 
             $trunc = explode('/@', $key);
-            if($trunc[0] == $base){
-                $innerRoute = $this->f3['ROUTES'][$index][3][$this->method][0];
-                $first = explode('->',$innerRoute);
-                $this->action = $first[1];
-                $second = explode('\\', $first[0]);
-                $third = explode('Controller', $second[2]);
-                $this->controller = strtolower($third[0]);
+            if ($trunc[0] == $base) {
+                $innerRoute         = $this->f3['ROUTES'][$index][3][$this->method][0];
+                $first              = explode('->', $innerRoute);
+                $this->action       = $first[1];
+                $second             = explode('\\', $first[0]);
+                $third              = explode('Controller', $second[2]);
+                $this->controller   = strtolower($third[0]);
                 break;
             }
         }
