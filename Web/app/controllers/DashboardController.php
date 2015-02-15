fdec6676578ffe756617494d2d948529d0559bcf
Mathieu <mathieu.letyrant@gmail.com>
2015-02-15 15:52:12 +0100

    Back - PSR Dashboard ^^

--- Web/app/controllers/DashboardController.php
+++ Web/app/controllers/DashboardController.php
@@ -1,24 +1,24 @@
 <?php
+
 namespace APP\CONTROLLERS;
 
-use APP\MODELS\User as User;
 use APP\MODELS\Store as Store;
 
-class DashboardController extends BaseController{
-
+class DashboardController extends BaseController
+{
 
     /*
-    *   GET dashboard
-    * */
-    public function index(){
-
+     * GET /dashboard
+     */
+    public function index()
+    {
         $user = $this->loggedRequire();
 
-        $stores = Store::where('user_id','=',$user->id )->with('trackerstores')->get();
+        $stores = Store::where('user_id', '=', $user->id)->with('trackerstores')->get();
 
         $this->render(true, [
-            'user'=>$user,
-            'stores'=>$stores
+            'user'   => $user,
+            'stores' => $stores
         ]);
     }
 
