de3ffdae8e94784e4b289e5fa163ef1a3c4c8229
Patrick HENG <patrick.heng@hetic.net>
2015-02-15 17:08:59 +0100

    Front#Web - Add button on dashboard

---
+++ Web/integration/dashboard.php
@@ -0,0 +1,102 @@
+<?php include('header.php'); ?>
+<?php include('aside.php'); ?>
+
+<!-- MAIN CONTENT -->
+<div id="container">
+	<div id="dashboard">
+		<h1>Dashboard</h1>
+		<ul class="cardList clearfix">
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Adress</h3>
+				<span><a href="#">http://zalendo.fr</a></span>
+				<span>20/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Resume</h3>
+				<span><a href="#">http://monster.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			<li>
+				<h3>Email Adress</h3>
+				<span><a href="#">http://meetic.fr</a></span>
+				<span>15/02/15</span>
+				<div class="moreInfo">
+					<span>+</span>
+				</div>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+
+<?php include('footer.php'); ?>
