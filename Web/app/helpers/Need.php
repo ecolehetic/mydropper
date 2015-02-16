e10a3e6764d0c0ae558b4298180b35567e6165b9
Mathieu <mathieu.letyrant@gmail.com>
2015-02-16 11:19:34 +0100

    Back - Check level in NeedHelper

--- Web/app/helpers/Need.php
+++ Web/app/helpers/Need.php
@@ -2,27 +2,31 @@
 
 namespace APP\HELPERS;
 
+use APP\MODELS\User;
+
 /**
  * Class Need
- * TODO Add Role rules
  * @package APP\HELPERS
  */
 class Need extends BaseHelper
 {
 
-    const LOGGED = 'logged';
-    const UNLOGGED = 'unlogged';
-    const UNLOGGED_MESSAGE = "Vous ne pouvez pas accéder à cette page étant connecté.";
-    const LOGGED_MESSAGE = "Vous devez être connecté pour accéder à cette page";
+    const LOGGED                = 'logged';
+    const UNLOGGED              = 'unlogged';
+    const LEVEL                 = 'level';
+    const UNLOGGED_MESSAGE      = "Vous ne pouvez pas accéder à cette page étant connecté.";
+    const LOGGED_MESSAGE        = "Vous devez être connecté pour accéder à cette page.";
+    const LEVEL_MESSAGE         = "Vous n'avez pas le level minimum pour accéder à cette page.";
 
-    private $choose = null;
-    private $redirect = null;
-    private $user = null;
+    private $choose     = null;
+    private $redirect   = null;
+    private $user       = null;
+    private $level      = null;
 
     /**
      * Register the choose and add Path in the instance
      *
-     * @param $path
+     * @param string $path
      *
      * @return $this
      */
@@ -37,7 +41,7 @@ class Need extends BaseHelper
     /**
      * Register the choose and add Path in the instance
      *
-     * @param $path
+     * @param string $path
      *
      * @return $this
      */
@@ -45,6 +49,21 @@ class Need extends BaseHelper
     {
         $this->choose = 'unlogged';
         $this->redirect = $path;
+
+        return $this;
+    }
+
+    /*
+     * Minimum Level
+     * Use for Admin page
+     *
+     * @param int $level
+     *
+     * @return $this
+     */
+    public function minimumLevel($level)
+    {
+        $this->level = $level;
 
         return $this;
     }
@@ -85,8 +104,40 @@ class Need extends BaseHelper
     private function error($value)
     {
         $fMessage = new FlashMessage();
-        $fMessage->set(($value === self::LOGGED) ? self::LOGGED_MESSAGE : self::UNLOGGED_MESSAGE);
+        switch($value){
+            case self::LOGGED:
+                $fMessage->set(self::LOGGED_MESSAGE);
+                break;
+            case self::UNLOGGED:
+                $fMessage->set(self::UNLOGGED_MESSAGE);
+                break;
+            case self::LEVEL:
+                $fMessage->set(self::LEVEL_MESSAGE);
+                break;
+            default: break;
+        }
         $this->f3->reroute($this->redirect, true);
+    }
+
+    /*
+     * Check if the user have the minimum level Required
+     *
+     * @param int $level
+     */
+    private function checkLevel($level)
+    {
+        if($this->testLogged() === false){
+            $user = $this->f3->get('SESSION.user');
+            $role = User::find($user->id)->roles()->get();
+
+            if($role->level > $level){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
     }
 
     /**
@@ -98,10 +149,17 @@ class Need extends BaseHelper
             $this->error(self::LOGGED);
             return;
         }
+
         if($this->choose === self::UNLOGGED && $this->testLogged() === false){
             $this->error(self::UNLOGGED);
             return;
         }
+
+        if($this->checkLevel($this->level) === false){
+            $this->error(self::LEVEL);
+            return;
+        }
+
         if($this->user === true && $this->testLogged() === false){
             return $this->f3->get('SESSION.user');
         }
