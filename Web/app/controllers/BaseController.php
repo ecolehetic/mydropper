9573a8b295f74b972a8ff825363c9c00a41b5fff
NCLSNDR <hello@nclsndr.com>
2015-02-17 21:20:25 +0100

    Admin prefix support in urlGenerator

--- Web/app/controllers/BaseController.php
+++ Web/app/controllers/BaseController.php
@@ -113,18 +113,25 @@ class BaseController
     }
 
     /**
-     * Generate URL with $path and $params
+     * Generate URL with $path, GET $params and $admin prefix
      *
      * @param string $path
      * @param array $params
+     * $admin bool default FALSE
      *
      * @return string
      */
-    protected function urlGenerator($path, $params = [])
+    protected function urlGenerator($path, $params = [], $is_admin = false)
     {
         $hive = $this->f3->hive();
         $host = $hive['HEADERS']['Host'];
-        $url = 'http://'.$host.$path;
+        if($is_admin){
+            $prefix = '/admin';
+        }else{
+            $prefix = '';
+        }
+
+        $url = 'http://'.$host.$prefix.$path;
 
         foreach ($params as $param) {
             $url .= $param.'/';
