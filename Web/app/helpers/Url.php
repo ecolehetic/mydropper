18456fccaa046c8007e55b12863707b7a6d79307
Mathieu <mathieu.letyrant@gmail.com>
2015-02-18 10:33:25 +0100

    Back - Add GoogleUrl Helper

--- Web/app/helpers/Url.php
+++ Web/app/helpers/Url.php
@@ -8,10 +8,12 @@ class Url extends BaseHelper
      * Generate URL with $path, GET $params and $admin prefix
      *
      * @param string $path
-     * @param array $params
-     * $admin bool default FALSE
+     * @param array  $params
+     * @param bool   $is_admin
      *
      * @return string
+     * @internal param default $bool FALSE $is_admin
+     *
      */
     public function generate($path, $params = [], $is_admin = false)
     {
