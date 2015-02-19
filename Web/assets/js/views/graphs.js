636b73c31a6741f6eb23afd924848e8f3a3aad71
Patrick HENG <patrick.heng@hetic.net>
2015-02-19 11:19:17 +0100

    Front#Web - Continue charts improvements

--- Web/assets/js/views/graphs.js
+++ Web/assets/js/views/graphs.js
@@ -50,17 +50,6 @@ var Graph = {
 				seriesBarDistance: 10
 			};
 
-			/*var responsiveOptions = [
-				['screen and (max-width: 640px)', {
-					seriesBarDistance: 5,
-					axisX: {
-						labelInterpolationFnc: function (value) {
-							return value[0];
-						}
-					}
-				}]
-			];*/
-
 			new Chartist.Bar(selector, data, options);
 		}
 	},
--- Web/integration/tracking.php
+++ Web/integration/tracking.php
@@ -1,90 +1,97 @@
 <?php include('header.php'); ?>
 <?php include('aside.php'); ?>
 
-<!-- MAIN CONTENT -->
-<div class="container">
-	<div id="tracking">
-		<h1>Tracking</h1>
-        <ul id="tabs">
-            <li class="selected">Category 1</li><li>
-                Patrick 2</li><li>
-                Mongolyto 2</li><li>
-                SahshaetPikachu 2</li><li>
-                Mes impots</li><li>
-                Lettres de motivation</li><li>
-                Yoyoyoyouoyo 2</li><li>
-                Category 2</li><li>
-                Category 3</li>
-        </ul>
-        <!--<div id="line-graph" class="line-graph"></div>-->
+    <!-- MAIN CONTENT -->
+    <div class="container">
+        <div id="tracking">
+            <h1>Tracking</h1>
+            <ul id="tabs">
+                <li class="selected">Category 1</li><li>
+                    Patrick 2
+                </li><li>
+                    Mongolyto 2
+                </li><li>
+                    SahshaetPikachu 2
+                </li><li>
+                    Mes impots
+                </li><li>
+                    Lettres de motivation
+                </li><li>
+                    Yoyoyoyouoyo 2
+                </li><li>
+                    Category 2
+                </li><li>
+                    Category 3
+                </li>
+            </ul>
+            <!--<div id="line-graph" class="line-graph"></div>-->
 
-        <div class="categoryGraphContainer clearfix">
-            <div class="deltaGraphContainer">
-                <div class="inputGroup">
-                    <label>From : </label>
-                    <input type="date" id="from"/>
+            <div class="categoryGraphContainer clearfix">
+                <div class="deltaGraphContainer">
+                    <div class="inputGroup">
+                        <label>From : </label>
+                        <input type="date" id="from"/>
+                    </div>
+                    <div class="inputGroup">
+                        <label>To : </label>
+                        <input type="date" id="from"/>
+                    </div>
                 </div>
-                <div class="inputGroup">
-                    <label>To : </label>
-                    <input type="date" id="from"/>
+                <div class="categoryGraph ct-chart">
                 </div>
             </div>
-            <div class="categoryGraph ct-chart">
-            </div>
+
+
+            <ul id="snippetGraphList">
+                <li>
+                    <div class="snippetDetails">
+                        <h2>Snippets 1</h2>
+
+                        <p>Create at 12/02/2015</p>
+                    </div>
+
+                    <div class="clickRateGraph ct-chart .ct-square">
+                    </div>
+                    Click rate
+
+                    <div class="snippetGraph ct-chart"></div>
+                </li>
+                <li>
+                    <div class="snippetDetails">
+                        <h2>Snippets 1</h2>
+
+                        <p>Create at 12/02/2015</p>
+                    </div>
+
+                    <div class="clickRateGraphContainer">
+                        <div class="clickRateGraph ct-chart .ct-square">
+                        </div>
+                        Click rate
+                    </div>
+
+                    <div class="snippetGraphContainer clearfix">
+                        <div class="snippetGraph ct-chart"></div>
+                    </div>
+                </li>
+                <li>
+                    <div class="snippetDetails">
+                        <h2>Snippets 1</h2>
+
+                        <p>Create at 12/02/2015</p>
+                    </div>
+
+                    <div class="clickRateGraphContainer">
+                        <div class="clickRateGraph ct-chart .ct-square">
+                        </div>
+                        Click rate
+                    </div>
+
+                    <div class="snippetGraphContainer clearfix">
+                        <div class="snippetGraph ct-chart"></div>
+                    </div>
+                </li>
+            </ul>
         </div>
-
-
-        <ul id="snippetGraphList">
-            <li>
-                <div class="snippetDetails">
-                    <h2>Snippets 1</h2>
-                    <p>Create at 12/02/2015</p>
-                </div>
-
-                <div class="clickRateGraphContainer">
-                    <div class="clickRateGraph ct-chart .ct-square">
-                    </div>
-                    Click rate
-                </div>
-
-                <div class="snippetGraphContainer clearfix">
-                    <div class="snippetGraph ct-chart"></div>
-                </div>
-            </li>
-            <li>
-                <div class="snippetDetails">
-                    <h2>Snippets 1</h2>
-                    <p>Create at 12/02/2015</p>
-                </div>
-
-                <div class="clickRateGraphContainer">
-                    <div class="clickRateGraph ct-chart .ct-square">
-                    </div>
-                    Click rate
-                </div>
-
-                <div class="snippetGraphContainer clearfix">
-                    <div class="snippetGraph ct-chart"></div>
-                </div>
-            </li>
-            <li>
-                <div class="snippetDetails">
-                    <h2>Snippets 1</h2>
-                    <p>Create at 12/02/2015</p>
-                </div>
-
-                <div class="clickRateGraphContainer">
-                    <div class="clickRateGraph ct-chart .ct-square">
-                    </div>
-                    Click rate
-                </div>
-
-                <div class="snippetGraphContainer clearfix">
-                    <div class="snippetGraph ct-chart"></div>
-                </div>
-            </li>
-        </ul>
-	</div>
-</div>
+    </div>
 
 <?php include('footer.php'); ?>
