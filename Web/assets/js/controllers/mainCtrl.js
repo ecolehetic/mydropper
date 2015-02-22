a49922ccbeb6c6af4d768360f955442a48e599ac
Patrick HENG <hengpatrick.pro@gmail.com>
2015-02-22 23:42:25 +0100

    Font#Web - Changing regex for checking url

--- Web/assets/js/controllers/mainCtrl.js
+++ Web/assets/js/controllers/mainCtrl.js
@@ -103,12 +103,7 @@ $(document).ready(function() {
 	});
 
 	function validateUrl(str) {
-		var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-		'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-		'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-		'(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+		var pattern = new RegExp("^((http|https):\/\/){1}(www[.])?([a-zA-Z0-9]|-)+([.][a-zA-Z0-9(-|\/|=|?)?]+)+$"); // fragment locator
 		if(!pattern.test(str)) {
 			return false;
 		}
@@ -117,5 +112,5 @@ $(document).ready(function() {
 		}
 	}
 	// Init Datepickers
-	$('.datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
+	$('#birthdayDP').datepicker({ dateFormat: 'mm-dd-yy' });
 });
