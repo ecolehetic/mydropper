c827788c5858b21aac76b3cf0286cad37c70a604
Mathieu <mathieu.letyrant@gmail.com>
2015-02-24 19:59:51 +0100

    Back - Prepare page Chrome / HowItWorks

--- Web/app/controllers/IndexController.php
+++ Web/app/controllers/IndexController.php
@@ -13,16 +13,43 @@ use MyDropper\Models\Category;
 class IndexController extends BaseController
 {
 
+    /**
+     * GET /
+     */
     public function index()
     {
         $this->render(true);
     }
 
+    /**
+     * GET /how-are-you
+     */
     public function who_we_are()
     {
         $this->render(true);
     }
 
+    /**
+     * GET /chrome-extension
+     */
+    public function chromeExtension()
+    {
+        $this->need->logged('/users/login')->execute();
+
+        $this->render(true);
+    }
+
+    /**
+     * GET /how-it-works
+     */
+    public function howItWorks()
+    {
+        $this->render(true);
+    }
+
+    /**
+     * GET /admin
+     */
     public function admin_index()
     {
         $this->need->logged('/users/login')->minimumLevel(9)->user()->execute();
@@ -38,8 +65,13 @@ class IndexController extends BaseController
         ]);
     }
 
+    /**
+     * GET /debug
+     */
     public function debug()
     {
+        $this->need->logged('/users/login')->minimumLevel(9)->execute();
+
         $this->render('debug.twig', [
 
         ]);
