a4ffcf7db73c6589d6ded135f547f61a2a82b52f
Patrick HENG <hengpatrick.pro@gmail.com>
2015-02-24 20:14:48 +0100

    Front#Web - Changing little things to fit answer designer feedback

--- ChromeExtension/js/models/model.js
+++ ChromeExtension/js/models/model.js
@@ -4,9 +4,14 @@ var Model = {
 	logIn : function(usr, pwd, callback){
 		$.post( "http://mydropper.mathieuletyrant.com/api/connect", { username : usr, password : pwd }, "json")
 			.done(function(response) {
-				console.log(response);
+				if(response.success){
+					localStorage.setItem("myDropperUser", JSON.stringify(response));
+				}else {
+					alert(response.message);
+				}
+
 			})
-			.fail(function(){
+			.fail(function(response){
 				alert('Username / Password not correct.\nPlease try again')
 			});
 
