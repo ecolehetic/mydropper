9cd38aee47ea5b328cd8f536682fd3502618b9d0
Mathieu <mathieu.letyrant@gmail.com>
2015-02-25 21:54:00 +0100

    Back - Clean HistoryController

--- Web/app/controllers/HistoryController.php
+++ Web/app/controllers/HistoryController.php
@@ -19,11 +19,7 @@ class HistoryController extends BaseController
     {
         $user = $this->need->logged('/users/login')->user()->execute();
 
-        //$stores = Store::where('user_id', '=', $user->id)->with('trackerstores')->get();
-        $stores = Store::where('user_id', '=', $user->id)->with('categories')->get();
         $trackers = TrackerStore::where('user_id', '=', $user->id)->with('stores')->orderBy('created_at')->get();
-        //var_dump($trackers[0]->stores->label);
-        //die();
 
         $this->render(true, [
             'trackers' => $trackers
