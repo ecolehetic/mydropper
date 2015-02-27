5725797337e4c0f4394c1813d7f07c0cdda21c21
Mathieu <mathieu.letyrant@gmail.com>
2015-02-27 23:51:31 +0100

    Back - Page 404 and MysqlConnection

--- Web/index.php
+++ Web/index.php
@@ -17,12 +17,19 @@ $f3->config('app/config/config.ini');
 $f3->config('app/config/routes.ini');
 
 /*
+ * Init 404
+ */
+$f3->set('ONERROR', function ($f3) {
+    echo $f3->get('TWIG')->render('404.twig');
+});
+
+/*
  * Twig
  */
 new Twig($f3->get('UI'), [
-    'debug'         => $f3->get('TWIG_DEBUG'),
-    'cache'         => $f3->get('CACHE'),
-    'auto_reload'   => $f3->get('TWIG_AUTORELOAD')
+    'debug'       => $f3->get('TWIG_DEBUG'),
+    'cache'       => $f3->get('CACHE'),
+    'auto_reload' => $f3->get('TWIG_AUTORELOAD')
 ]);
 
 /*
